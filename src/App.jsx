ae8aa7410d5d78f268f24028be5c4d3a5a921fd5
Lorik Shabani <lorikshabani@Loriks-MBP.fritz.box>
2025-10-21 22:52:41 +0200

    finished movie app

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import { Routes, Route } from 'react-router'
 import Home from './pages/Home'
 import Favorites from './pages/Favorites'
 import NavBar from './components/NavBar'
+import { MovieProvider } from './contexts/MovieContext'
 
 function App() {
 
   return (
-    <div>
-
+    <MovieProvider>
     <NavBar />
    <main className='main-content'>
       <Routes>
@@ -17,7 +17,7 @@ function App() {
         <Route path='/favorites' element={<Favorites />} />
       </Routes>
    </main>
-   </div>
+   </MovieProvider>
   )
 }
 
